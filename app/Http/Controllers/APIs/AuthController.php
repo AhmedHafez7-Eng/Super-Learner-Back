acfa265bc19bca91a9023eef743342e54f76a21f
AhmedHafez7-Eng <ahmedhafezoffic@gmail.com>
2022-02-28 02:22:28 +0200

    Users Auth System Controller

---
+++ app/Http/Controllers/APIs/AuthController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\APIs;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'fname' => 'required|string|min:2|max:70',
+            'lname' => 'required|string|min:2|max:70',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'phone' => 'required|numeric|digits:11',
+            'address' => 'required|string|max:255',
+            'b_date' => 'required| date',
+            'role' => 'required',
+        ]);
+
+        $user = User::create([
+            'fname' => $validatedData['fname'],
+            'lname' => $validatedData['lname'],
+            'email' => $validatedData['email'],
+            'b_date' => $validatedData['b_date'],
+            'phone' => $validatedData['phone'],
+            'address' => $validatedData['address'],
+            'password' => Hash::make($validatedData['password']),
+            'role' => $validatedData['role'],
+        ]);
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ], 201);
+
+        // $resposne = [
+        //     'user' => $user,
+        //     'token' => $token
+        // ];
+        // return response($resposne, 201);
+    }
+
+    public function login(Request $request)
+    {
+
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        // Check For Email
+
+        $user = User::where('email', $fields['email'])->first();
+
+        // Check Password
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Email or Password are Invalid!'
+            ], 401);
+        }
+
+        $token = $user->createToken('myAppToken')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ], 201);
+
+        // $resposne = [
+        //     'user' => $user,
+        //     'token' => $token
+        // ];
+
+        // return response($resposne, 201);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'Logged Out'
+        ];
+    }
+}
